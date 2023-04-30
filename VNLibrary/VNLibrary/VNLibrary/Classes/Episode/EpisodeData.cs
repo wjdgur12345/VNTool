aab4923a03894a2bf567bb0320ce133d6b0450f6
wjdgur12345 <wjdgur12345@gmail.com>
2023-04-30 19:09:42 +0900

    구조 변경

---
+++ VNLibrary/VNLibrary/VNLibrary/Classes/Episode/EpisodeData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace VNLibrary
+{
+	public abstract class EpisodeDataBase
+	{
+		public abstract string EventType { get; }
+		public abstract string Content { get; }
+	}
+
+	public class BattleEpisodeData : EpisodeDataBase
+	{
+        public override string EventType => "Battle";
+        public override string Content => _targetContent;
+
+        private string _targetContent = string.Empty;
+
+		public BattleEpisodeData(string _Target)
+		{
+			_targetContent = _Target;
+		}
+    }
+
+    public class StoryEpisodeData : EpisodeDataBase
+    {
+
+        public override string EventType => "Story";
+        public override string Content => _targetContent;
+
+        private string _targetContent = string.Empty;
+
+        public StoryEpisodeData(string _Target)
+        {
+            _targetContent = _Target;
+        }
+    }
+
+    public class StoryAndBattleEpisodeData : EpisodeDataBase
+    {
+
+        public override string EventType => "StoryAndBattle";
+        public override string Content => _targetContent;
+
+        private string _targetContent = string.Empty;
+
+        public StoryAndBattleEpisodeData(string _Target)
+        {
+            _targetContent = _Target;
+        }
+    }
+
+    public class RewordEpisodeData : EpisodeDataBase
+    {
+
+        public override string EventType => "Reword";
+        public override string Content => _targetContent;
+
+        private string _targetContent = string.Empty;
+
+        public RewordEpisodeData(string _Target)
+        {
+            _targetContent = _Target;
+        }
+    }
+
+}
+
---
+++ VNLibrary/VNLibrary/VNLibrary/Classes/Util/IVNSerialize.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace VNLibrary.Util
+{
+    /// <summary>
+    /// 데이터 직렬화가 가능한 클래스에 붙여서 사용합니다.
+    /// </summary>
+    public interface IVNSerialize
+	{
+        
+	}
+}
+
